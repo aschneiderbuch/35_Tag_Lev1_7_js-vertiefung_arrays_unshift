235c485b9e2d97aa14946d286ac2b370a678c741
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-31 13:14:43 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 console.log("test");
+
+let obst = ["Apfel", "Birne"];
+
+const deutscheGerichte = ["Speckkuchen", "Thüringer Rostbratwurst", "Quarkkeulchen", "Sauerbraten"];
+deutscheGerichte.unshift("Pizza", "Nudeln", "Salat", obst, )
+
+console.log(deutscheGerichte);
+console.table(deutscheGerichte);
